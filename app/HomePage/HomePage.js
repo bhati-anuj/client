077d8310749246648864da15423435ff4f2f29a2
Kamal051998 <kamalpanwar192@gmail.com>
2023-12-30 14:40:05 +0530

    dynamically imported section 1&2

--- app/HomePage/HomePage.js
+++ app/HomePage/HomePage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import HeroSection from './HeroSection'
 import Header from '../components/Header'
-import Card from '../components/Card'
 import Footer from '../components/Footer'
 import SecondHeader from '../components/SecondHeader'
-import Section1 from './Section1'
-import Section2 from './Section2'
+import dynamic from 'next/dynamic'
+
+
 import Link from 'next/link'
 import CategorySection from './CategorySection'
+const Section1 = dynamic(() => import('./Section1'))
+const Section2 = dynamic(() => import('./Section2'))
 
 
 const HomePage = () => {
